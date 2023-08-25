7378910041f47d178eb261b26dad5fc8bc275fcf
AlexandrZolotukhin <zoloto.alex08@yandex.ru>
2023-08-25 21:20:14 +0300

    Add project files.

---
+++ IDA_Class_Work_UML_2/!_Main.cpp
@@ -0,0 +1,65 @@
+﻿//class template version 5.0 //07.07.2023//
+#include <iostream>
+#include <string>
+#include <windows.h>
+#include <time.h>
+#include <algorithm>
+#include <conio.h>
+//My own headers
+#include "Service functions.h"
+
+
+// Class Work 25.08.2023 -------------------------------------------	
+
+// 1. Activity dia
+
+// 2. State-chart dia
+
+
+/*
+
+
+
+*/
+
+
+
+
+
+//Task 1
+void Task_1()
+{
+
+}
+//Task 2
+void Task_2()
+{
+	
+}
+//Task 3
+void Task_3()
+{
+
+}
+
+
+// MAIN ------- MAIN ------- MAIN ------- MAIN ------- MAIN --------
+int main()
+{
+srand(time(NULL));
+		
+	
+Task_1();
+Task_2();
+Task_3();
+
+
+
+
+
+std::cout << "\n\n";
+//system("pause");	
+_getch();
+}
+
+
